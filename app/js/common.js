bbda9c4523f139b230465e170e3a690a56aa21bc
Petr Petukhov <kotov0991@gmail.com>
2018-06-15 13:29:44 +0300

    font face observer edit sass

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,18 @@
+window.onload = function () {
+	var font = new FontFaceObserver('Font');
+  
+	font.load().then(function () {
+	  document.documentElement.className += "Font";
+	});
+	font.load()
+	  .then(function(result) {
+		console.log('Font is available');
+	  })
+	  .catch(function(error) {
+		console.log('Font is not available');
+	  });
+  };
+
 $(function() {
 
 	
