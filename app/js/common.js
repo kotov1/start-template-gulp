a6491aaae1d9b58666e47ea57860a31540ad4e06
Petr Petukhov <kotov0991@gmail.com>
2018-07-12 11:14:48 +0300

    optimiztion 1.2

--- app/js/common.js
+++ app/js/common.js
@@ -1,18 +1,3 @@
-window.onload = function () {
-	var font = new FontFaceObserver('Font');
-  
-	font.load().then(function () {
-	  document.documentElement.className += "Font";
-	});
-	font.load()
-	  .then(function(result) {
-		console.log('Font is available');
-	  })
-	  .catch(function(error) {
-		console.log('Font is not available');
-	  });
-  };
-
 $(function() {
 
 	
